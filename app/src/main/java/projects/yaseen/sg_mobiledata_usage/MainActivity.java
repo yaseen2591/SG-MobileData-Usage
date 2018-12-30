815f4bd2e154133ba1efeb5fe46a4a524cc3fa2c
Mohammed Yaseen <yaseen@homage.co>
2018-12-30 21:51:45 +0800

    Created Android studio app

---
+++ app/src/main/java/projects/yaseen/sg_mobiledata_usage/MainActivity.java
@@ -0,0 +1,13 @@
+package projects.yaseen.sg_mobiledata_usage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
